21ac19b4db9d80096c87838cac0f21815476b0eb
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2018-09-22 15:43:48 +0300

    Add katakana (first 5 rows)
    And minor style corrections.

---
+++ learn_katakana.py
@@ -0,0 +1,31 @@
+import random
+
+
+row_a = [('а', 'ア'), ('и', 'イ'), ('у', 'ウ'), ('э', 'エ'), ('о', 'オ')]
+row_ka = [('ка', 'カ'), ('ки', 'キ'), ('ку', 'ク'), ('кэ', 'ケ'), ('ко', 'コ')]
+row_sa = [('са', 'サ'), ('ши', 'シ'), ('су', 'ス'), ('сэ', 'セ'), ('со', 'ソ')]
+row_ta = [('та', 'タ'), ('чи', 'チ'), ('цу', 'ツ'), ('тэ', 'テ'), ('то', 'ト')]
+row_na = [('на', 'ナ'), ('ни', 'ニ'), ('ну', 'ヌ'), ('нэ', 'ネ'), ('но', 'ノ')]
+
+
+kana = row_a + row_ka + row_sa + row_ta + row_na
+
+
+def learn():
+    try:
+        while kana:
+            syllable = random.choice(kana)
+            kana.remove(syllable)
+
+            print(syllable[0])
+            input()
+
+            print(syllable[1])
+            input()
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    learn()
