8b4a82e99ab66402c0016a035d26352b4c62c5b1
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2018-10-12 18:20:43 +0300

    words: Pass lesson num to input
    Lets use the common lessons script for convenience.

--- nihongo/learn_words.py
+++ nihongo/learn_words.py
@@ -1,6 +1,7 @@
+import sys
 import core
 
 
 if __name__ == '__main__':
-    words = core.read_words('nihongo/lesson1.txt')
+    words = core.read_words('nihongo/lesson{}.txt'.format(sys.argv[1]))
     core.learn(words)
--- nihongo/learn_words2.py
+++
@@ -1,6 +0,0 @@
-import core
-
-
-if __name__ == '__main__':
-    words = core.read_words('nihongo/lesson2.txt')
-    core.learn(words)
