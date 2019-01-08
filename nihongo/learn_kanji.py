274a284c4ecab634e381a2c783dd72c66656d2a6
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2019-01-08 14:47:42 +0300

    kanji: Fix a typo in the file name

--- nihongo/learn_kanji.py
+++ nihongo/learn_kanji.py
@@ -4,6 +4,6 @@ import sys
 
 if __name__ == '__main__':
     search_tags = sys.argv[1].split(',') if len(sys.argv) > 1 else None
-    words = core.read_words('nihongo/kandji300.txt', set(search_tags))
+    words = core.read_words('nihongo/kanji300.txt', set(search_tags))
 
     core.learn(words)
