4621f35651dc7270229ff86c2c448478ea2b43e0
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2019-01-11 19:15:22 +0300

    kanji: Fix crash if no search args were provided

--- nihongo/learn_kanji.py
+++ nihongo/learn_kanji.py
@@ -3,7 +3,7 @@ import sys
 
 
 if __name__ == '__main__':
-    search_tags = sys.argv[1].split(',') if len(sys.argv) > 1 else None
-    words = core.read_words('nihongo/kanji300.txt', set(search_tags))
+    search_tags = set(sys.argv[1].split(',')) if len(sys.argv) > 1 else set()
+    words = core.read_words('nihongo/kanji300.txt', search_tags)
 
     core.learn(words)
