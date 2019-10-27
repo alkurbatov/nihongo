057243755f5cf11076d38e172c005d5e0b38d886
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2019-10-27 20:40:17 +0300

    Rename kanji300.txt
    As now we have many more kanji in it.
    
    Signed-off-by: Alexander Kurbatov <alkurbatov@virtuozzo.com>

--- nihongo/learn_kanji.py
+++ nihongo/learn_kanji.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     core.clear_screen()
 
     search_tags = set(sys.argv[1].split(',')) if len(sys.argv) > 1 else set()
-    words = core.read_words('nihongo/kanji300.txt', search_tags)
+    words = core.read_words('nihongo/kanji.txt', search_tags)
 
     core.learn(words)
