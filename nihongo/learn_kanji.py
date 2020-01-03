631dd077254f93afa98f33937e8789a299a52751
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2020-01-03 12:33:39 +0300

    feat: Support intervals as input args
    Now it is possible to specify kanji groups as interval,
    e.g. 330-410.
    
    Signed-off-by: Alexander Kurbatov <alkurbatov@virtuozzo.com>

--- nihongo/learn_kanji.py
+++ nihongo/learn_kanji.py
@@ -2,10 +2,31 @@ import core
 import sys
 
 
+def parse_args(args):
+    if not len(args):
+        return set()
+
+    if ',' in args[0]:
+        return set(args[0].split(','))
+
+    if '-' in args[0]:
+        interval = args[0].split('-', maxsplit=1)
+
+        tags = set()
+        start = int(interval[0])
+        while start <= int(interval[1]):
+            tags.add(str(start))
+            start += 10
+
+        return tags
+
+    return set()
+
+
 if __name__ == '__main__':
     core.clear_screen()
 
-    search_tags = set(sys.argv[1].split(',')) if len(sys.argv) > 1 else set()
+    search_tags = parse_args(sys.argv[1:])
     words = core.read_words('nihongo/kanji.txt', search_tags)
 
     core.learn(words)
